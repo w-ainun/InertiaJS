32dc27af759b18bfc69379c5484217730feb01bc
icangg877 <161398329+icangg877@users.noreply.github.com>
2025-05-31 22:26:38 +0700

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,21 +11,47 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            'Kue Basah',
-            'Kue Kering',
-            'Kue Modern',
-            'Gorengan',
-            'Minuman',
-            'Puding',
-        ];
+        [
+            'name' => 'Kue Basah',
+            'image_url' => '/img/categories/kue-basah.png',
+            'description' => 'Aneka kue basah pilihan terbaik',
+        ],
+        [
+            'name' => 'Kue Kering',
+            'image_url' => '/img/categories/kue-kering.png',
+            'description' => 'Beli banyak makin hemat!',
+        ],
+        [
+            'name' => 'Kue Modern',
+            'image_url' => '/img/categories/kue-modern.png',
+            'description' => 'Best Seller, MULAI DARI 3.500!',
+        ],
+        [
+            'name' => 'Gorengan',
+            'image_url' => '/img/categories/gorengan.png',
+            'description' => 'paling laris!',
+        ],
+        [
+            'name' => 'Minuman',
+            'image_url' => '/img/categories/minuman.png',
+            'description' => 'NIkmati Kesegaran hanya dengan 10.000!',
+        ],
+        [
+            'name' => 'Puding',
+            'image_url' => '/img/categories/puding.png',
+            'description' => 'Paling diminati',
+        ],
+    ];
 
-        foreach ($categories as $name) {
-            DB::table('categories')->insert([
-                'name' => $name,
-                'slug' => Str::slug($name),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
+        foreach ($categories as $category) {
+        DB::table('categories')->insert([
+            'name' => $category['name'],
+            'slug' => Str::slug($category['name']),
+            'image_url' => $category['image_url'],
+            'description' => $category['description'],
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
     }
 }
